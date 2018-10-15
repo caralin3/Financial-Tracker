e22045d4b44b77bf4b59860116e25433c2281086
caralin3 <caralin3@gmail.com>
2018-10-15 16:59:47 -0400

    Initial commit of files

---
+++ src/firebase/index.ts
@@ -0,0 +1,12 @@
+import * as auth from './auth';
+import * as db from './firebase';
+import * as firebase from './firebase';
+import { currentUser, usersCollection } from './firebase';
+
+export {
+  auth,
+  currentUser,
+  db,
+  firebase,
+  usersCollection,
+};
