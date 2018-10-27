f2b55b23b61a46a4a4770b352b8148ac56b2fd69
caral <cxs4754@rit.edu>
2018-10-26 22:48:40 -0400

    Removed unnecessary code

--- src/firebase/fb.ts
+++ src/firebase/fb.ts
@@ -3,24 +3,6 @@ import 'firebase/auth';
 import 'firebase/firestore';
 import { devConfig, prodConfig } from './config';
 
-// const prodConfig = {
-//   apiKey: YOUR_API_KEY,
-//   authDomain: YOUR_AUTH_DOMAIN,
-//   databaseURL: YOUR_DATABASE_URL,
-//   projectId: YOUR_PROJECT_ID,
-//   storageBucket: '',
-//   messagingSenderId: YOUR_MESSAGING_SENDER_ID,
-// };
-
-// const devConfig = {
-//   apiKey: YOUR_API_KEY,
-//   authDomain: YOUR_AUTH_DOMAIN,
-//   databaseURL: YOUR_DATABASE_URL,
-//   projectId: YOUR_PROJECT_ID,
-//   storageBucket: '',
-//   messagingSenderId: YOUR_MESSAGING_SENDER_ID,
-// };
-
 const config = process.env.NODE_ENV === 'development'
   ? devConfig
   : prodConfig;
