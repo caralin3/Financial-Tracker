60fc26b5b9b4d7769580aa4b43583435a0cec81a
caralin3 <caralin3@gmail.com>
2019-05-01 09:52:33 -0400

    Fix total calculations in data table

--- src/util/calculator.ts
+++ src/util/calculator.ts
@@ -4,4 +4,4 @@ export const calcPercent = (frac: number, total: number) => (frac / total) * 100
 
 export const getArraySum = (acc: number, val: number) => acc + val;
 
-export const getArrayTotal = (arr: any[]) => (arr.length ? formatNumberArray(arr).reduce(getArraySum) : 0);
+export const getArrayTotal = (arr: any[]) => arr.length ? formatNumberArray(arr).reduce(getArraySum) : 0;
